bb2527c529847bbf3d9491895bfa25fb3e305562
WMaxZimmerman <william.m.zimmerman@gmail.com>
2017-11-14 16:29:32 -0600

    Added the template to the source

---
+++ Misc/CommandAndConquer.CLI.Template/CommandAndConquer.CLI.Template/obj/Debug/ProjectTemplates/CommandAndConquer.CLI/Controllers/ExampleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CommandAndConquer.CLI.Attributes;
+
+namespace $safeprojectname$.Controllers
+{
+    [CliController("ex", "The is an example of a controller that you can setup.")]
+    public static class ExampleController
+    {
+        [CliCommand("hello", "This is a simple hello world command. It is to be used a reference when creating your own commands.")]
+        public static void HelloWorld(string firstName, string lastName = null)
+        {
+            Console.WriteLine($"Hello, {firstName} {lastName}");
+        }
+    }
+}
---
+++ Misc/CommandAndConquer.CLI.Template/CommandAndConquer.CLI.Template/obj/Debug/ProjectTemplates/CommandAndConquer.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using CommandAndConquer.CLI.Core;
+
+namespace $safeprojectname$
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Processor.ProcessArguments(args);
+        }
+    }
+}
