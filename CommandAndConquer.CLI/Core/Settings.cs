3a359068243a35e1c82720deda7cd64a35769e3b
Max Zimmerman <william.m.zimmerman@gmail.com>
2017-11-24 18:39:14 -0600

    Allowing the App.config values to be optional and defaulting to the ones provided in the documentation

--- CommandAndConquer.CLI/Core/Settings.cs
+++ CommandAndConquer.CLI/Core/Settings.cs
@@ -4,7 +4,7 @@ namespace CommandAndConquer.CLI.Core
 {
     public static class Settings
     {
-        public static string HelpString = ConfigurationManager.AppSettings["helpString"];
-        public static string ArgumentPrefix = ConfigurationManager.AppSettings["argumentPrefix"];
+        public static string HelpString = ConfigurationManager.AppSettings["helpString"] ?? "?";
+        public static string ArgumentPrefix = ConfigurationManager.AppSettings["argumentPrefix"] ?? "--";
     }
 }
